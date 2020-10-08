5832f2360a0066cc8e0afc9c39687cec1d2cb1e4
VladKoch_ <vlad2002kochurko@gmail.com>
2020-10-08 20:31:44 +0300

    feat: implement morse decoder function

--- src/index.js
+++ src/index.js
@@ -38,7 +38,41 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-
+    if (expr.length % 10 != 0) throw Error;
+    let stringAll = '';
+    let letters = [];
+    let str = '';
+    for (let i = 0, j = 0; i <= expr.length; i++) {
+        if (i % 10 == 0 && i != 0) {
+          letters[j] = str;
+          j++;
+          str = '';
+        }
+        str += expr[i];
+    }
+    for (let i = 0; i < letters.length; i++) {
+        let oneLetter = letters[i].split('');
+         if (oneLetter[0] === '*') {
+              stringAll += ' ';
+              continue;
+        }
+        let code = '';
+        let j = 0;
+        while (oneLetter[j] != 1) {
+            j++;
+        }
+        oneLetter = oneLetter.splice(j);
+        for (let k = 0; k < oneLetter.length; k = k + 2) {
+            if (oneLetter[k] === oneLetter[k + 1]) {
+                code += '-';
+            }
+            else {
+                code += '.';
+            }
+        }
+        stringAll += MORSE_TABLE[code];
+    }
+    return stringAll;
 }
 
 module.exports = {
